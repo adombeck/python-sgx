b14dceaa1b583f23d80f4a53dc260674a204fe80
Adrian Dombeck <adrian.dombeck@rub.de>
2017-02-28 14:36:01 +0100

    Add main() in attestation.c for testing without Python
    Conflicts:
    	sgx/attestation/attestation.c

--- sgx/attestation/attestation.c
+++ sgx/attestation/attestation.c
@@ -1,21 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#include "sgx_tseal.h"
-
-#include <Python.h>
-
-#define SECRET_LEN 256
-#define PLAIN_TEXT_LEN 256
-
-// Global data
-sgx_sealed_data_t* g_sealed_buf;
-uint8_t* g_secret;
-uint8_t* g_plain_text;
-uint8_t* g_unsealed_secret;
-uint8_t* g_unsealed_plain_text;
-
 
 #include "sgx_uae_service.h"
 
@@ -29,4 +13,14 @@ void get_extended_epid_group_id(uint32_t* p_group_id)
         return;
     }
     fprintf(stderr, "Successfully got EPID group id %u\n", *p_group_id);
+}
+
+// This is only for testing
+int main()
+{
+    fprintf(stderr, "Getting EPID group id...");
+    uint32_t group_id;
+    get_extended_epid_group_id(&group_id);
+
+    return 0;
 }
