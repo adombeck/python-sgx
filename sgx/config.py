79942952af8c282c7f1e8b21d7c33b8080e7908a
Adrian Dombeck <adrian.dombeck@rub.de>
2017-04-13 15:49:49 +0200

    Improve error message in sgx/config.py

--- sgx/config.py
+++ sgx/config.py
@@ -4,17 +4,19 @@ from os import path
 
 PROJECT_GIT_DIR = path.dirname(sys.modules[__name__].__file__)
 GRAPHENE_DIR = path.realpath(path.join(PROJECT_GIT_DIR, "graphene"))
-HOME_DIR = path.expanduser("~/.sgx")
+KEY_DIR = "/etc/python-sgx/"
+DATA_DIR = "/var/lib/python-sgx/"
 
 
 class UnsupportedSystemException(Exception):
-    def __init__(self):
-        message = "Unsupported OS. Currently this package can only be used on Ubuntu 14.04 and Ubuntu 16.04."
+    def __init__(self, distribution, version):
+        os = "%s %s" % (distribution, version)
+        message = "Unsupported OS %r. Currently this package can only be used on Ubuntu 14.04 and Ubuntu 16.04." % os
         super(Exception, self).__init__(message)
 
 distribution, ubuntu_version = platform.linux_distribution()[:2]
 if distribution != "Ubuntu":
-    raise UnsupportedSystemException()
+    raise UnsupportedSystemException(distribution, ubuntu_version)
 
 if ubuntu_version == "14.04":
 #    PYTHON_VERSION = "3.4"
@@ -23,7 +25,7 @@ elif ubuntu_version == "16.04":
 #    PYTHON_VERSION = "3.5"
     LIBPROTOBUF_VERSION = "9"
 else:
-    raise UnsupportedSystemException()
+    raise UnsupportedSystemException(distribution, ubuntu_version)
 
 # XXX: Currently we require Python 3.5. Install it with `apt install python3.5` on Ubuntu 14.04.
 PYTHON_VERSION = "3.5"
