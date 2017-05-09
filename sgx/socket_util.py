949cd20cd59eebe583e43726877f47a6795f91ac
Adrian Dombeck <adrian.dombeck@rub.de>
2017-05-09 15:20:13 +0200

    Use exceptions instead of assert statements

--- sgx/socket_util.py
+++ sgx/socket_util.py
@@ -1,17 +1,15 @@
 import socket
 import logging
 
+from sgx.exceptions import ConnectionClosedError
+
 logger = logging.getLogger(__name__)
-
-
-class ConnectionClosedException(Exception):
-    pass
 
 
 def receive_bytes(sock: socket.socket, max_size=4096):
     received_raw = sock.recv(max_size).strip()
     if not received_raw:
-        raise ConnectionClosedException()
+        raise ConnectionClosedError()
 
     logger.debug("Received (size: %s): %s", len(received_raw), received_raw)
     return received_raw
--- sgx/util.py
+++ sgx/util.py
@@ -1,5 +1,7 @@
 import os
 
+from sgx.exceptions import UnexpectedLengthError
+
 from sgx.config import KEY_DIR
 
 
@@ -7,3 +9,11 @@ def get_attester_public_key():
     pubkey_path = os.path.join(KEY_DIR, "attester_public.key")
     with open(pubkey_path) as pubkey_file:
         return bytes.fromhex(pubkey_file.read())
+
+
+def check_json(data, type_, length=None):
+    if not isinstance(data, type_):
+        raise TypeError("Received JSON is of type %r, expected type %r" % (type(data), type_))
+
+    if length and len(data) != length:
+        raise UnexpectedLengthError("Received JSON %r has length %r, expected length %r" % (type_, data, length))
